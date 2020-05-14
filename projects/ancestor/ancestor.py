94f50d5974db5b5521625fe768dc4caf4ee30690
Indigo Richards <thedomesticdingo@gmail.com>
2020-05-14 15:24:54 -0700

    Day 3 commit
    finished social.py

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,6 +1,5 @@
 from util import Stack, Queue
 
-
 def earliest_ancestor(ancestors, starting_node):
     q = Queue()
     path = [starting_node]
@@ -9,16 +8,14 @@ def earliest_ancestor(ancestors, starting_node):
     while q.size() > 0:
         currentPath = q.dequeue()
         newPath = []
-        changed = False
 
         for node in currentPath:
             for ancestor in ancestors:
                 if ancestor[1] == node:
                     newPath.append(ancestor[0])
-                    changed = True
                     q.enqueue(newPath)
 
-        if changed is False:
+        if len(newPath) <= 0:
             if currentPath[0] == starting_node:
                 return -1
             else:
